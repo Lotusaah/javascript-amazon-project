5465b36ae22f0608a57fd0585723152fa94e3b36
vddav <vddavis@icloud.com>
2024-03-07 11:06:35 -0500

    easy excersice done

--- javascript-amazon-project-main - Excersies/scripts/amazon.js
+++ javascript-amazon-project-main - Excersies/scripts/amazon.js
@@ -58,7 +58,7 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
-    const productId = button.dataset.productId;
+    const {productId} = button.dataset;
 
     const quantityAmount = document.querySelector(`.js-quantity-selector-${productId}`).value;
 
@@ -76,8 +76,8 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       matchingItem.quantity += Number(quantityAmount);
     } else {
       cart.push({
-        productId: productId,
-        quantity: 1
+        productId,
+        quantity: Number(quantityAmount)
       });
     };
 
