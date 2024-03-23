bdbe1fc9cf4206aa8aaad28ca765e0faec3a2e18
vddav <vddavis@icloud.com>
2024-03-23 19:36:08 -0400

    Completed 15k

--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -1,10 +1,10 @@
 import {cart, removeFromCart, updateQuantity, totalQuantityCount, updateDeliveryOption} from '../../data/cart.js';
 import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
-import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
 import {renderPaymentSummary} from './paymentSummary.js';
+import {renderCheckoutHeader} from './checkoutHeader.js';
 
 export function renderOrderSummary() {
 
@@ -115,10 +115,9 @@ export function renderOrderSummary() {
       const productId = link.dataset.productId;
       removeFromCart(productId);
 
-      const container = document.querySelector(`.js-cart-item-container-${productId}`);
-      container.remove();
-
+      renderOrderSummary();
       renderPaymentSummary();
+      renderCheckoutHeader();
     });
   });
 
@@ -132,6 +131,7 @@ export function renderOrderSummary() {
   document.querySelectorAll('.save-quantity-link').forEach((link) => {
     link.addEventListener('click', () => {
       updateItemQuantity(link);
+      renderPaymentSummary();
     });
 
     const productId = link.dataset.productId;
@@ -140,6 +140,7 @@ export function renderOrderSummary() {
       if (event.key === "Enter") {
         event.preventDefault();
         updateItemQuantity(link);
+        renderPaymentSummary();
       }
     });
   });
--- javascript-amazon-project-main/scripts/checkout/paymentSummary.js
+++ javascript-amazon-project-main/scripts/checkout/paymentSummary.js
@@ -1,4 +1,4 @@
-import {cart} from '../../data/cart.js';
+import {cart, totalQuantityCount} from '../../data/cart.js';
 import {getProduct} from '../../data/products.js';
 import {getDeliveryOption} from '../../data/deliveryOptions.js'
 import {formatCurrency} from '../utils/money.js';
@@ -23,14 +23,13 @@ export function renderPaymentSummary() {
 
   console.log(totalCents);
 
-
   const paymentSummaryHTML = `
     <div class="payment-summary-title">
       Order Summary
     </div>
 
     <div class="payment-summary-row">
-      <div>Items (3):</div>
+      <div>Items (${totalQuantityCount()}):</div>
       <div class="payment-summary-money">
         $${formatCurrency(productPriceCents)}
       </div>
